5cc1c1076a8631e7ae5d565db72b95a32deb2d92
VovaSles <vovaswebdev@gmail.com>
2022-03-24 00:17:29 +0200

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+export default function ButtonAppBar() {
   return (
-    <div>NavBar</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            WEATHER
+          </Typography>
+          <Box  pt={1}>
+          <Button color="inherit" component={Link} to={'/'}>SEARCH</Button>
+          </Box>
+          <Box  pt={1}>
+          <Button color="inherit" component={Link} to={'/favorites'}>FAVORITES</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default NavBar
