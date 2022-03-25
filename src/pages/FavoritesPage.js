a4c60edf7843e77f12ff31813e6910de7fcc7864
VovaSles <vovaswebdev@gmail.com>
2022-03-26 01:36:56 +0300

    make responsive

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeFromFavoritesAction } from '../redux/reducer';
+import { Card, Button, Row, Col } from 'react-bootstrap'
+import { Container } from '@mui/material';
 
 const FavoritesPage = () => {
   const dispatch = useDispatch();
   const favorites = useSelector(state => state.favorites);
+  const weather = useSelector(state => state.cityWeather);
 
-  const removeCity = (city) =>{
+  const removeCity = (city) => {
     dispatch(removeFromFavoritesAction(city.Key))
   }
 
 
-  
+
   return (
     <>
-        {console.log(favorites)}
-      { favorites.map(city => (
-      <div key={city.Key}>{city.LocalizedName}<button onClick={() => removeCity(city)}>Remove</button></div>
-    ))}
+      <Container>
+        <Row xs={1} md={4} className="g-4 mt-5">
+          {favorites.map(city => (
+            <Col key={city.Key}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>{city.LocalizedName}</Card.Title>
+                  <h3>{weather.Temperature.Metric.Value} {weather.Temperature.Metric.Unit}</h3>
+                  <h2>{weather.WeatherText}</h2>
+                  <Button className='m-2' variant="warning" onClick={() => removeCity(city)}>Remove</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
+      </Container>
     </>
-   
+
   )
 }
 
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Card, Button, Badge, Container, Row, Col ,Form} from 'react-bootstrap'
+import { Card, Button, Badge, Container, Row, Col, Form } from 'react-bootstrap'
 
 const SearchPage = () => {
     const dispatch = useDispatch();
@@ -27,61 +27,67 @@ const SearchPage = () => {
 
 
             <Container>
-            
-                <Form className='  mt-5' style={{ width: '80rem' }}>
-                    <Form.Group className="mb-3 mt-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Control placeholder="SEARCH" />
-                    </Form.Group>
-                </Form>
-                {/* <input value={inputValue} onChange={e => setInputValue(e.target.value)} />  */}
-                {/* <button onClick={() => dispatch(fetchAutocompleteOptions(inputValue))}>get options</button> */}
-            
+                <Row>
+                    <Col xs={12} md={12}>
+                        <Form className=' m-auto mt-5'>
+                            <Form.Group className="mb-3 mt-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Control placeholder="SEARCH" />
+                            </Form.Group>
+                        </Form>
+                        {/* <input value={inputValue} onChange={e => setInputValue(e.target.value)} />  */}
+                        {/* <button onClick={() => dispatch(fetchAutocompleteOptions(inputValue))}>get options</button> */}
 
 
 
 
-                <Card className='  mt-5' style={{ width: '80rem' }}>
-                    <Card.Body>
-                        <Card.Title>
-                            <Row className='mt-5'>
-                            <Col  md={{ span: 4 }}>
-                            <h2 className="text-center m-auto">
-                                {city.LocalizedName} 
-                                <Badge bg="secondary" className='m-3'>
-                                 {weather.Temperature.Metric.Value}
-                                 {weather.Temperature.Metric.Unit}
-                            </Badge>
-                            </h2>
-                         
-                            </Col>
-                            <Col md={{ span: 2, offset: 6 }}> <Button
-                                className=".justify-content-end"
-                                variant="primary"
-                                onClick={() => dispatch({ type: 'ADD_FAVORITE', payload: city })}>Add to Favorites</Button></Col>
-                            </Row>
-                        </Card.Title>
 
-                        <h1 className="text-center m-auto" >{weather.WeatherText}</h1>
+                        <Card className='  mt-5' >
+                            <Card.Body>
+                                <Card.Title>
+                                    <Row className='mt-5 justify-content-center'>
+                                        <Col  xs={12} md={{ span: 4 }}>
+                                            <h2 className="text-center m-auto">
+                                                {city.LocalizedName}
+                                                <Badge bg="secondary" className='m-3'>
+                                                    {weather.Temperature.Metric.Value}
+                                                    {weather.Temperature.Metric.Unit}
+                                                </Badge>
+                                            </h2>
+
+                                        </Col>
+                                        <Col  xs={{ span: 6, offset: 4 }} md={{ span: 2, offset: 6 }}>
+                                            <Button
+                                            variant="primary"
+                                            onClick={() => dispatch({ type: 'ADD_FAVORITE', payload: city })}>Like it</Button></Col>
+                                    </Row>
+                                </Card.Title>
+
+                                <h1 className="text-center m-auto" >{weather.WeatherText}</h1>
 
 
-                        {console.log(daysWeather.DailyForecasts)}
+                                {console.log(daysWeather.DailyForecasts)}
 
-                        <Row xs={1} md={5} className="g-5 m-3">
-                            {daysWeather.DailyForecasts.map((d, idx) => (
-                                <Col key={d.EpochDate}>
-                                    <Card className="text-center">
-                                        <Card.Body>
-                                            <Card.Title>{getWeekDat(d.Date)}</Card.Title>
-                                            <h5> {d.Temperature.Maximum.Value}
-                                                {d.Temperature.Maximum.Unit}
-                                            </h5>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </Card.Body>
-                </Card>
+                                <Row xs={1} md={5} className="g-5 m-3">
+                                    {daysWeather.DailyForecasts.map((d, idx) => (
+                                        <Col key={d.EpochDate}>
+                                            <Card className="text-center"  bg="warning">
+                                                <Card.Body>
+                                                    <Card.Title>{getWeekDat(d.Date)}</Card.Title>
+                                                    <h5> {d.Temperature.Maximum.Value}
+                                                        {d.Temperature.Maximum.Unit}
+                                                    </h5>
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
+                                    ))}
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                </Row>
+
+
             </Container>
         </>
     )
